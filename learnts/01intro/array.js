340aa17fbb33985e4667118c79d981df2786b825
varunrnc <barunpandit143@gmail.com>
2025-08-11 23:40:52 +0530

    learn typescript

---
+++ learnts/01intro/array.js
@@ -0,0 +1,25 @@
+// const names: string[] = [];
+// names.push("Varun");  // no error
+// console.log(names[0]); 
+// Readonly
+// const names: readonly string[] = ["Varun"];
+// names.push("Pandit");
+// type interface
+// const numbers = [1, 2, 3];
+// numbers.push(4);
+// numbers.push("5");
+// let head: number = numbers[0];
+// console.log(head);
+// typescript tuples
+// define our tuple
+// let ourTuple: [number, boolean, string];
+// initialize correctly
+// ourTuple = [5, true, "Coding God was here"];
+// console.log(ourTuple);
+// object
+var car = {
+    type: "Toyota",
+    model: "Corolla",
+    year: 2009
+};
+console.log(car);
